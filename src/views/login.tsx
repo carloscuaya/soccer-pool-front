b0a4b58f5c17a91fcc1124f6e5be18387229ad35
Carlitos Cuaya <52793533+carloscuaya@users.noreply.github.com>
2026-05-01 21:58:32 -0600

    :zap: add values to improve performance in safari browser

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -65,8 +65,8 @@ function Login() {
         <div className="bg-background text-on-surface min-h-screen selection:bg-secondary-container selection:text-on-secondary-container">
             {/* Cloud-like Gradient Background */}
             <div className="fixed inset-0 z-0 pointer-events-none overflow-hidden">
-                <div className="absolute -top-24 -left-24 w-[600px] h-[600px] rounded-full bg-secondary-fixed/30 blur-[120px]"></div>
-                <div className="absolute top-1/2 -right-48 w-[800px] h-[800px] rounded-full bg-primary-fixed/10 blur-[140px]"></div>
+                <div className="absolute -top-24 -left-24 w-[600px] h-[600px] rounded-full bg-secondary-fixed/30 blur-[60px]"></div>
+                <div className="absolute top-1/2 -right-48 w-[800px] h-[800px] rounded-full bg-primary-fixed/10 blur-[80px]"></div>
                 <div className="absolute bottom-0 left-0 w-full h-64 bg-gradient-to-t from-surface-container-low to-transparent"></div>
             </div>
             {/* Main Navigation Shell (TopNavBar - Suppressed for Login context, but following identity) */}
